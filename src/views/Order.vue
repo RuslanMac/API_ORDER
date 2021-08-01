50a2bab253b118d56f67affe263300a68134b875
RuslanMac <ruslan-karamurzanov@mail.ru>
2021-08-01 12:56:38 +0600

    api key

--- src/views/Order.vue
+++ src/views/Order.vue
@@ -18,8 +18,10 @@
   </div>
 </template>
 <script>
-import OrderDetails from "../components/OrderDetails.vue"; 
+import OrderDetails from "../components/OrderDetails.vue";
 import apiClient from "@/services/apiClient";
+var publicKey = "38cd79b5f2b2486d86f562e3c43034f8";
+var privateKey = "8e49ff607b1f46e1a5e8f6ad5d312a80";
 export default {
   data() {
     return {
@@ -43,20 +45,19 @@ export default {
           console.log("There error" + " " + error);
         });
     },
-    getRequestToken() {
+
+    getAccessToken() {
       apiClient
         .getRequestToken()
         .then((response) => (this.requestToken = response.data.RequestToken))
         .catch(() => {
           console.log("There is error");
         });
-    },
-    getAccessToken() {
       var tsum = this.getSHA1();
       //var passwords = sha1(password);
       var requestToken1 = this.requestToken;
       apiClient
-        .getAccessToken(requestToken1, process.env.VUE_APP_PUBLIC_KEY, tsum)
+        .getAccessToken(requestToken1, publicKey, tsum)
         .then((response) => {
           this.accessToken = response.data.AccessToken;
           this.getOrder();
@@ -65,7 +66,7 @@ export default {
     getSHA1() {
       var crypto = require("crypto");
       var shasum = crypto.createHash("sha1");
-      var password = this.requestToken + process.env.VUE_APP_PRIVATE_KEY;
+      var password = this.requestToken + privateKey;
       var tsum = shasum.update(password);
       tsum = shasum.digest("hex");
       return tsum;
