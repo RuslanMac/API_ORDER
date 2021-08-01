a2c51530697d1554694787f0e6b7c4379eec59b5
RuslanMac <ruslan-karamurzanov@mail.ru>
2021-08-01 12:58:56 +0600

    fix api keys

--- src/services/apiClient.js
+++ src/services/apiClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create( {
-    baseURL: 'http://localhost:8083/',
+    baseURL: 'http://localhost:8080/',
     headers: {
         "Access-Control-Allow-Origin": "*"
     }
