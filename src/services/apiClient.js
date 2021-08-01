3d4f6ba8da12d4a987d60dbd8196a226e0bff070
RuslanMac <ruslan-karamurzanov@mail.ru>
2021-08-01 12:32:32 +0600

    new improve

--- src/services/apiClient.js
+++ src/services/apiClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create( {
-    baseURL: '',
+    baseURL: 'http://localhost:8080/',
     headers: {
         "Access-Control-Allow-Origin": "*"
     }
@@ -10,17 +10,17 @@ const apiClient = axios.create( {
 export default {
    async getOrders(oauth_token) {
         console.log("sp");
-        return await apiClient.get('http://localhost:8080/orders', {
+        return await apiClient.get('orders', {
             params: {
                 oauth_token: oauth_token 
             }
         });
     }, 
     async getRequestToken() {
-        return await apiClient.get('http://localhost:8080/oauth/requesttoken');
+        return await apiClient.get('oauth/requesttoken');
     }, 
    async getAccessToken(oauth_token, username, password) {
-        return await apiClient.get('http://localhost:8080/oauth/accesstoken', {
+        return await apiClient.get('oauth/accesstoken', {
             params: {
                 oauth_token: oauth_token,
                 grant_type: 'api',
